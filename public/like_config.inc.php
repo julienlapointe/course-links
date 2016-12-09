5be1e6ab971df0a7c2412c46e768d23e67814a0b
Julien LaPointe <julien.lapointe@me.com>
2016-12-08 23:12:51 -0500

    Update like_config.inc.php

--- public/like_config.inc.php
+++ public/like_config.inc.php
@@ -2,7 +2,7 @@
 $mysql_host = 'localhost'; // Enter MySQL Server host. Eg: localhost
 $mysql_user = 'root'; // Enter MySQL Server user. Eg: root
 $mysql_pass = 'root'; // Enter MySQL User password.
-$mysql_base = 'link_sharing_v12'; // Enter MySQL Database. Eg: slike
+$mysql_base = 'course_links'; // Enter MySQL Database. Eg: slike
 	
 $mysql_conn = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_base);
 // $mysql_sel_db = mysqli_select_db(,$mysql_conn);
